01851f94de288fb786b0b7ae0796ec0d1d98e302
Tyrone Saulon <tyrone.saulon@hotmail.com>
2018-11-09 13:26:46 -0500

    Adding clock encapsulation

--- src/App.js
+++ src/App.js
@@ -3,22 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clock: this.props.clock
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          {this.state.clock}
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
